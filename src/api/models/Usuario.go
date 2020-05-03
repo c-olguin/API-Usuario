9c84edd3685f6ca6ad1e38e631f8ea1b05ee69dc
jagatica <julieta.gatica@mercadolibre.com>
2020-05-03 20:41:41 -0300

    agrego institucionID

--- src/api/models/Usuario.go
+++ src/api/models/Usuario.go
@@ -2,23 +2,23 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
-) 
+)
 
 type Usuario struct {
 	gorm.Model
-	ID					 uint				 `json:"id"`
-	Nombre               string               `json:"nombre"`
-	Apellido             string               `json:"apellido"`
-	Email            	 string               `json:"email"`
-	Username             string               `json:"username"`
-	Password             string               `json:"password"`
-	Rol             	 string               `json:"rol"`
-	Imagen_url           string               `json:"imagen_url"`
-	Telefono             string               `json:"telefono"`
-	Especialidad         string               `json:"especialidad"`
+	ID            uint   `json:"id"`
+	Nombre        string `json:"nombre"`
+	Apellido      string `json:"apellido"`
+	Email         string `json:"email"`
+	Username      string `json:"username"`
+	Password      string `json:"password"`
+	Rol           string `json:"rol"`
+	Imagen_url    string `json:"imagen_url"`
+	Telefono      string `json:"telefono"`
+	Especialidad  string `json:"especialidad"`
+	InstitucionID int64  `json:"institucion_id,omitempty"`
 }
 
 func (Usuario) TableName() string {
 	return "usuarios"
 }
-
