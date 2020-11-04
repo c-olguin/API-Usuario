4e54d384c5647942b75b5dc1bbd633d8836292ed
Julieta Gatica <jag81295@gmail.com>
2020-11-04 20:28:02 -0300

    actualizacion de libs

--- src/api/config/databaseConfig.go
+++ src/api/config/databaseConfig.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 type DatabaseConfig struct {
@@ -54,32 +54,28 @@ func init() {
 	var err error
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true&autoCommit=true", databaseConfig.Username, databaseConfig.Password, databaseConfig.Hostname, databaseConfig.Database)
-	bibliotecaDigitalDB, err = gorm.Open("mysql", dsn)
+	bibliotecaDigitalDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Println("db_connection_error database:", databaseConfig.Database)
 		panic(err)
 	}
-
-	bibliotecaDigitalDB.DB().SetMaxIdleConns(databaseConfig.MaxIddleConn)
-	bibliotecaDigitalDB.DB().SetMaxOpenConns(databaseConfig.MaxOpenConn)
-	bibliotecaDigitalDB.DB().SetConnMaxLifetime(databaseConfig.ConnMaxLifetime)
 }
 
 func BibliotecaDigitalDB() (*gorm.DB, error) {
+	var err error
 
-	var error error
-
-	stats := bibliotecaDigitalDB.DB().Stats()
+	db, err := bibliotecaDigitalDB.DB()
+	stats := db.Stats()
 
 	if stats.OpenConnections >= 10 {
-		err := fmt.Errorf("number of connections exceeded: %v", stats.OpenConnections)
+		err = fmt.Errorf("number of connections exceeded: %v", stats.OpenConnections)
 		return nil, err
 	}
 
-	err := bibliotecaDigitalDB.DB().Ping()
+	err = db.Ping()
 	if err != nil {
 		log.Println("error connect biblioteca_digital db", err)
 	}
 
-	return bibliotecaDigitalDB, error
+	return bibliotecaDigitalDB, err
 }
